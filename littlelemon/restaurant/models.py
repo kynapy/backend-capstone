8040bf86f1f2d3cc04139ccf006e2c522cf87aa6
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-05-23 01:53:28 +0800

    Add models and DRF dependency

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(max_length=5)
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    numGuests = models.IntegerField(max_length=6)
+    bookingDate = models.DateField()
