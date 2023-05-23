2aa349f7165a9a69bcc25c8da8fe27d6ded8c9d0
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-05-24 04:26:00 +0800

    Update test scripts

--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 from restaurant.models import Menu, Booking
+from restaurant.serializers import MenuSerializer
 
 class MenuViewTest(TestCase):
-    def setup(self):
+    def setUp(self):
         Menu.objects.create(title='IceCream', price=5, inventory=100)
         Menu.objects.create(title='Cake', price=10, inventory=50)
 
     def test_getall(self):
-        self.setup()
         response = self.client.get('/restaurant/menu/')
+        menu = Menu.objects.all()
+        serializer = MenuSerializer(menu, many=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data, serializer.data)
